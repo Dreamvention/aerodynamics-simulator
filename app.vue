d5f90600137bdd739ea32710f7fb8c650bc825e0
D.Z. <dmitriyzhuk@gmail.com>
2026-06-19 10:58:40 -0700

    feat: add main app component with layout

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gradient-to-r from-slate-90 to-slate-98">
+    <NuxtLayout />
+  
+      </div>
+  </template>
+  
+      <!J/Auto imports from #app
+&#10;
