ad4058071bb7c3c0bfd1a9712befe2e00f1ce69c
Dmitriy Zhuk <dmitriy@dreamvention.com>
2026-06-21 13:24:20 +0300

    refactor: restructure project - move spaces/web to app, spaces/api to api, remove root Nuxt config

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen bg-gradient-to-r from-slate-90 to-slate-98">
-    <NuxtLayout />
-  
-      </div>
-  </template>
-  
-      <!J/Auto imports from #app
-&#10;
